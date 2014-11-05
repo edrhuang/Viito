a17751b3025a744e6029651516360c5115d760fa
Edward Huang <edrhuang@gmail.com>
2014-11-05 12:52:21 -0800

    change to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
 	get 'songs/:playlist_id' => 'users#get_songs'
 
-	get '/auth/spotify/callback', to: 'welcome#spotify'
+	get 'auth/spotify/callback', to: 'welcome#spotify'
 
 	get 'sessions' => 'sessions#create'
 
